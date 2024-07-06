e13f827671d9b9660c2fc0ae98d1f1df6d6d6b69
Can Ngo <44318634+nncan99@users.noreply.github.com>
2024-07-06 16:29:54 +0700

    Magic 8 ball source code

--- src/index.js
+++ src/index.js
@@ -3,11 +3,76 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import userEvent from '@testing-library/user-event';
+
+const inputStyle = {
+  width: 235,
+  margin: 5
+}
+
+class MagicEightBall extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      userInput: '',
+      randomIndex: ''
+    };
+    this.ask = this.ask.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  ask(){
+    if (this.state.userInput){
+      this.setState({
+        randomIndex: Math.floor(Math.random()*20),
+        userInput: ''
+      });
+    }
+  }
+  handleChange(event){
+    this.setState({
+      userInput: event.target.value
+    });
+  }
+  render(){
+    const possibleAnswers = [
+      'It is certain',
+      'It is decidedly so',
+      'Without a doubt',
+      'Yes, definitely',
+      'You may rely on it',
+      'As I see it, yes',
+      'Outlook good',
+      'Yes',
+      'Signs point to yes',
+      'Reply hazy try again',
+      'Ask again later',
+      'Better not tell you now',
+      'Cannot predict now',
+      'Concentrate and ask again',
+      "Don't count on it",
+      'My reply is no',
+      'My sources say no',
+      'Most likely',
+      'Outlook not so good',
+      'Very doubtful'
+    ];
+    const answer = possibleAnswers[this.state.randomIndex];
+    return (
+      <div>
+        <input type="text" value={this.state.userInput} onChange={this.handleChange} style={inputStyle} /><br />
+        <buton onClick={this.ask}>Ask the Magic Eight Ball!!</buton>
+        <br />
+        <h3>Answer:</h3>
+        <p>{answer}</p>
+      </div>
+    )
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    
   </React.StrictMode>
 );
 
